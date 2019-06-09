a6d4985be153f679a2a178f83be94621fe6cf179
Kelly Gibbens <gibbenskd@gmail.com>
2019-06-09 06:47:50 -0700

    cleanup

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,12 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    gifs: [],
   },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  mutations: {},
+  actions: {},
 });
