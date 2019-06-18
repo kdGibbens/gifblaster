de88a20add4106cbdb775e7d1fb2a1bb380ee205
Kelly Gibbens <gibbenskd@gmail.com>
2019-06-17 20:06:15 -0700

    added Axios, Rework img tag to set src from state in computed property.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="app">
-    <Home />
+    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
 
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 </style>
+
--- src/router.ts
+++ src/router.ts
@@ -7,5 +7,11 @@ Vue.use(Router);
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+  ],
 });
